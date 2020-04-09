6ef7833691b4374b81f77ceb72c8ce5984ea821a
Moinuddinsheikh <51946699+Moinuddinsheikh@users.noreply.github.com>
2020-04-09 20:44:04 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $('#splash-logo').toggleClass('popanim');
+
+    setTimeout(function(){
+        $('#splash').fadeOut();
+        setTimeout(function(){ 
+            $('html').css('overflow','unset');
+            setTimeout(function(){ 
+                $('#intro-name').css('animation','name-anim 3s linear forwards');
+                
+            },200);
+        },500);
+    },1600);
+    
+    $('#coverpage-list a').click(function(e){     
+        e.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+     
+});
