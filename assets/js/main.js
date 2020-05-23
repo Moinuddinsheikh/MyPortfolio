6522330530332143f373115c8f9f74902c18e6e2
Moinuddinsheikh <51946699+Moinuddinsheikh@users.noreply.github.com>
2020-05-23 14:06:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,1 @@
-$(window).ready(function() {
-
-    $('#splash-logo').toggleClass('popanim');
-
-    setTimeout(function(){
-        $('#splash').fadeOut();
-        setTimeout(function(){ 
-            $('html').css('overflow-y','unset');
-        },500);
-    },1600);
-    
-    $('#coverpage-list a').click(function(e){     
-        e.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-    });
-     
-});
+$(window).ready(function(){$("#splash-logo").toggleClass("popanim");setTimeout(function(){$("#splash").fadeOut();setTimeout(function(){$("html").css("overflow-y","unset")},500)},1600);$("#coverpage-list a").click(function(a){a.preventDefault();$("html,body").animate({scrollTop:$(this.hash).offset().top},500)})});
